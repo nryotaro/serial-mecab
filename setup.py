9eefbf23ea119356d8e5dc497798223faef3d663
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2020-01-03 11:26:06 +0900

    add tests

--- setup.py
+++ setup.py
@@ -1,20 +1,24 @@
+"""Metadata of the package."""
 from setuptools import setup, find_packages
 
 
-setup(name='serial-mecab',
-        version="0.0.1",
-        packages=find_packages(),
-        install_requires=[
-            ],
-        extras_require={
-            'test': [
-                'pytest'
-                ],
-            'dev': [
-                'ipython',
-                'python-language-server[all]'
-                ],
-            'doc': [             
-                'sphinx',
-                'sphinx_rtd_theme'
-                ]})
+setup(
+    name='serial-mecab',
+    version="0.0.1",
+    packages=find_packages(),
+    install_requires=[
+        'mecab-python3',
+        'joblib'
+    ],
+    extras_require={
+        'test': [
+            'pytest'
+        ],
+        'dev': [
+            'ipython',
+            'python-language-server[all]'
+        ],
+        'doc': [
+            'sphinx',
+            'sphinx_rtd_theme'
+        ]})
